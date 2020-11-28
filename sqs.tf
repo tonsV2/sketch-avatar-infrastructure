c55103169000197eac42f38bbaa7290ce0dd342d
sj <sebastianthegreatful@gmail.com>
2020-11-29 00:01:13 +0700

    Disable dead letter queue

--- sqs.tf
+++ sqs.tf
@@ -4,10 +4,12 @@ resource "aws_sqs_queue" "sqs_deadletter" {
 
 resource "aws_sqs_queue" "sqs" {
   name = "legacy2modern"
+/* TODO: If I enable this all messages goes to the dead letter queue
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.sqs_deadletter.arn
-    maxReceiveCount     = 4
+    maxReceiveCount = 4
   })
+*/
 }
 
 resource "aws_lambda_event_source_mapping" "sqs" {
