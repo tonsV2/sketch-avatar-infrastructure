7b66f5c1aa0048ac3cb60fc18ae8fcba57978df7
sj <sebastianthegreatful@gmail.com>
2020-11-29 18:38:02 +0700

    Create 200 initial avatars and run more of the commands concurrently

--- scripts/create_test_data.sh
+++ scripts/create_test_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGES=10
+IMAGES=200
 LABEL="Sketch\nAvatar\nID:{}"
 FILENAME="avatar-{}.png"
 
@@ -16,23 +16,21 @@ aws s3 mb s3://$LEGACY_BUCKET
 aws s3 mb s3://$MODERN_BUCKET
 
 ## Generate avatars
-seq $IMAGES | xargs -I '{}' convert -background lightblue -fill blue -size 165x70 -pointsize 18 -gravity center label:$LABEL /tmp/$FILENAME
+seq $IMAGES | xargs -P 4 -I '{}' convert -background lightblue -fill blue -size 165x70 -pointsize 18 -gravity center label:$LABEL /tmp/$FILENAME
 
 ## Store in S3
-seq $(($IMAGES / 2)) | xargs -I '{}' aws s3 cp /tmp/$FILENAME "s3://$LEGACY_BUCKET/image/$FILENAME"
-seq $(($IMAGES / 2 + 1)) $IMAGES | xargs -I '{}' aws s3 cp /tmp/$FILENAME "s3://$MODERN_BUCKET/avatar/$FILENAME"
+seq $(($IMAGES / 2)) | xargs -P 4 -I '{}' aws s3 cp /tmp/$FILENAME "s3://$LEGACY_BUCKET/image/$FILENAME"
+seq $(($IMAGES / 2 + 1)) $IMAGES | xargs -P 4 -I '{}' aws s3 cp /tmp/$FILENAME "s3://$MODERN_BUCKET/avatar/$FILENAME"
 
 ## Remove local avatar files
 rm /tmp/avatar-*.png
 
 # Get list of avatars from both buckets
-aws s3 ls s3://$LEGACY_BUCKET/image/ | cut -c32- | xargs -I '{}' echo "image/{}" >> /tmp/avatars.txt
+aws s3 ls s3://$LEGACY_BUCKET/image/ | cut -c32- | xargs -I '{}' echo "image/{}" > /tmp/avatars.txt
 aws s3 ls s3://$MODERN_BUCKET/avatar/ | cut -c32- | xargs -I '{}' echo "avatar/{}" >> /tmp/avatars.txt
 
-# Store in random order
-shuf < /tmp/avatars.txt | xargs -I '{}' sh -c "echo '{\"s3key\": \"{}\"}' | http $(terraform output api_url)"
-
-rm /tmp/avatars.txt
+# Update the database
+xargs -a /tmp/avatars.txt -I '{}' sh -c "echo '{\"s3key\": \"{}\"}' | http $(terraform output api_url)"
 
 # Show content of database
 http "$(terraform output api_url)"
