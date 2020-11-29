70ac77f887d7769f0caa02c8f3883ae7e267d560
sj <sebastianthegreatful@gmail.com>
2020-11-29 18:35:55 +0700

    Use 8 processes to submit messages to queue. This is not best practice, messages should be submitted in batch

--- scripts/populate_queue.sh
+++ scripts/populate_queue.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-LEGACY_BUCKET=sketch-legacy
+#TODO: Send messages in batch - https://docs.aws.amazon.com/cli/latest/reference/sqs/send-message-batch.html
 
-aws s3 ls s3://$LEGACY_BUCKET/image/ | cut -c32- | xargs -P 4 -I '{}' aws sqs send-message --queue-url "$(terraform output sqs_url)" --message-body 'image/{}'
+LEGACY_BUCKET=sketch-legacy
+CONCURRENT_PROCESSES=8
+
+aws s3 ls s3://$LEGACY_BUCKET/image/ | cut -c32- | xargs -P $CONCURRENT_PROCESSES -I '{}' aws sqs send-message --queue-url "$(terraform output sqs_url)" --message-body 'image/{}'
